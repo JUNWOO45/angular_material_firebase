210f338d63a59411f48e02e23b5743bde02f40a9
junwoo45 <sinyoorang@gmail.com>
2019-09-30 20:12:24 +0900

    Use Subject

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription} from 'rxjs';
 import { ProductsService } from './products.service';
 
 @Component({
   selector: 'products-component',
   templateUrl: './products.component.html'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   productName = '박준우';
   isDisabled = true;
-  products = [];
+  products = ['asb'];
+  private productsSubscription: Subscription;
 
   constructor(private productsService: ProductsService) { 
+    
     setTimeout(() => {
       this.isDisabled = false;
-      this.products = this.productsService.getProducts();
     }, 3000);
   }
 
   ngOnInit() {
+    this.products = this.productsService.getProducts();
+    this.productsSubscription = this.productsService.productsUpdated.subscribe(() => {
+      this.products = this.productsService.getProducts();
+    });
   }
 
   changeName() {
@@ -30,12 +36,18 @@ export class ProductsComponent {
     if(form.valid) {
       // this.products.push(form.value.productName);
       this.productsService.addProduct(form.value.productName);
+      
     }
+    // this.products = this.productsService.getProducts();
   }
 
   onRemoveProduct(product: string) {
     this.products = this.products.filter(p => {
       return p !== product;
     })
+  }
+
+  ngOnDestroy() {
+    this.productsSubscription.unsubscribe();
   }
 }
--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,11 +1,20 @@
+import { Subject } from 'rxjs';
+
 export class ProductsService {
   private products = ['책', '공', '꿈'];
+  productsUpdated = new Subject();
 
   addProduct(productName: string) {
     this.products.push(productName);
+    this.productsUpdated.next();
   }
 
   getProducts() {
     return [...this.products];
+  }
+
+  deleteProduct(productName: string) {
+    this.products = this.products.filter(p => p !== productName);
+    this.productsUpdated.next();
   }
 }
