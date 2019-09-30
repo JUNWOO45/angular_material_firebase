8d030ac1d28f8452d09a16a241c1d71aecea763e
junwoo45 <sinyoorang@gmail.com>
2019-09-30 20:55:44 +0900

    Use Angular Material button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,23 @@ import { ProductsService } from './products.service';
 import { HomeComponent } from './home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+// import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     ProductComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
+    // MatSliderModule
   ],
   providers: [ProductsService],
   bootstrap: [AppComponent]
