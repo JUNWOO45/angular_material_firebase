6ebd21c691be0dad31571edf928d3b145223a1b7
Parkjunwoo <sinyoorang@gmail.com>
2019-10-01 23:21:18 +0900

    Add mat-form-field & matInput to signup page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatSliderModule, MatCheckboxModule, MatIconModule } from '@angular/material'
+import { MatButtonModule, MatSliderModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material'
 
 @NgModule({
-  imports: [MatButtonModule, MatSliderModule, MatCheckboxModule, MatIconModule],
-  exports: [MatButtonModule, MatSliderModule, MatCheckboxModule, MatIconModule]
+  imports: [MatButtonModule, MatSliderModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatSliderModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule {}
