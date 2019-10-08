49ba06524872ff5c273b5f699b36b94ba5ec6b8e
junwoo45 <sinyoorang@gmail.com>
2019-10-08 20:11:12 +0900

    Prepare exercise data

---
+++ src/app/training/training.service.ts
@@ -0,0 +1,10 @@
+import { Exercise } from './exercise.model';
+
+export class TrainingService {
+  availableExercises: Exercise[] = [
+    { id: 'soccer', name: 'Soccer', duration: 180, calories: 120 },
+    { id: 'marathon', name: 'Marathon', duration: 240, calories: 200 },
+    { id: 'swimming', name: 'Swimming', duration: 80, calories: 110 },
+    { id: 'climing', name: 'Climing', duration: 60, calories: 80 }
+  ];
+}
