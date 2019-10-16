f33f8cc8ab7cb39b2641876a140da0acfecb7dfd
junwoo45 <sinyoorang@gmail.com>
2019-10-16 23:59:32 +0900

    Add snackbar for UX when users met wrong situation

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,13 +6,19 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { TrainingService } from '../training/training.service';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private isAuthenticated = false;
 
-  constructor(private router: Router, private afAuth: AngularFireAuth, private trainingService: TrainingService) {
+  constructor(
+    private router: Router, 
+    private afAuth: AngularFireAuth, 
+    private trainingService: TrainingService,
+    private _snackBar: MatSnackBar
+    ) {
 
   }
 
@@ -40,6 +46,7 @@ export class AuthService {
       })
       .catch(error => {
         console.log('error : ', error);
+        this.openSnackBar('회원가입에 실패했습니다. 다시 한 번 시도해주세요.', '확인');
       })
   }
 
@@ -52,14 +59,22 @@ export class AuthService {
     })
     .catch(error => {
       console.log('error : ', error);
+      this.openSnackBar('잘못된 이메일이나 비밀번호입니다.', '확인');
     })
   }
 
   logout() {
+    this.openSnackBar('로그인을 한 뒤 서비스를 이용할 수 있습니다.', '확인');
     this.afAuth.auth.signOut();
   }
 
   isAuth() {
     return this.isAuthenticated;
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 }
