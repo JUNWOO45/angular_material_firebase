afa50bf1421e07bc643e9ee17079feb5d8b16800
junwoo45 <sinyoorang@gmail.com>
2019-10-25 00:23:10 +0900

    Splitting up routes

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { MaterialModule } from '../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from '../app-routing.module';
-import { FormsModule } from '@angular/forms';
 
 import { TrainingComponent } from './training.component';
 import { CurrentTrainingComponent } from './current-training/current-training.component';
 import { NewTrainingComponent } from './new-training/new-training.component';
 import { PastTrainingComponent } from './past-training/past-training.component';
 import { StopTrainingComponent } from './current-training/stop-training.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -23,12 +19,9 @@ import { StopTrainingComponent } from './current-training/stop-training.componen
     StopTrainingComponent
   ],
   imports: [
-    CommonModule,
-    MaterialModule,
-    FlexLayoutModule,
-    FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
+    SharedModule
   ],
   exports: [],
   entryComponents: [StopTrainingComponent]
